e8e5cc14604b4a43e1b40ef4e4a6aa2c4e8f7f57
Xplay518 <gustavo.valencia@ucb.edu.bo.com>
2024-03-19 00:51:38 -0400

    1er commit original, el anterior era de prueba

---
+++ src/bowling.js
@@ -0,0 +1,8 @@
+class Game {
+    constructor() {
+      // Constructor de la clase Game
+    }
+  }
+  
+  module.exports = Game;
+  
---
+++ src/bowling.spec.js
@@ -0,0 +1,10 @@
+const Game = require('./bowling');
+
+describe('Bowling game', () => {
+  test('Gutter game scores 0', () => {
+    const g = new Game();
+    // Agregar código de prueba para verificar el juego en caso de 0 puntos
+  });
+
+  // Agregar más pruebas según sea necesario para otras funcionalidades del juego
+});
